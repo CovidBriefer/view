4881a3042f23f1692f920bcbd95904dcccdccbd8
flaaaps <julianwening4@gmail.com>
2021-06-01 21:19:38 +0200

    Handle onClick

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 
-interface Props {}
+interface Props {
+    onClick?: () => void
+}
 
-const Button: React.FC<Props> = ({ children }) => {
+const Button: React.FC<Props> = ({ children, onClick }) => {
     return (
         <button
+            onClick={() => onClick?.()}
             className="bg-primary py-2 px-20 text-primary-dark font-semibold tracking-tighter text-lg shadow-2xl"
             style={{ borderRadius: "3px", filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))" }}
         >
