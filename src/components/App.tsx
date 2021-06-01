d72a64d1b0c8efa1beda1d66dde9fbae43224ac9
flaaaps <julianwening4@gmail.com>
2021-06-02 01:58:59 +0200

    Show loading spinner while fetching config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,27 +2,46 @@ import React, { useEffect, useState } from "react"
 import { Route } from "react-router-dom"
 import { IonReactRouter } from "@ionic/react-router"
 
-import { Storage } from "@capacitor/storage"
-// import Loading from "./Loading"
 import Setup from "./pages/setup/Setup"
 import Home from "./pages/Home"
-import { IonApp, IonRouterOutlet } from "@ionic/react"
+import { IonApp, IonLoading, IonRouterOutlet } from "@ionic/react"
+import { Config, Page } from ".."
+import useStorage from "../hooks/useStorage"
 
 const App: React.FC = () => {
-    const [showSetup, setShowSetup] = useState(false)
+    // const [showSetup, setShowSetup] = useState(false)
+    const [setupPage, setSetupPage] = useState<Page | undefined | null>(null) // undefined == no setup required; null == n
+    // const [loading, setLoading] = useState()
+    const { data, loading: configLoading } = useStorage("config", 550)
+
     useEffect(() => {
-        Storage.get({ key: "config" }).then(res => {
-            const data: { setup: boolean } = JSON.parse(res.value!!)
-            if (!data.setup) setShowSetup(true)
-        })
-    }, [])
+        if (!configLoading && data?.value) {
+            const parsed: Config = JSON.parse(data?.value)
+            if (!parsed.setup.done) setSetupPage(parsed.setup.currentPage)
+        }
+    }, [data, configLoading])
     return (
         <IonApp>
-            <IonReactRouter>
-                <IonRouterOutlet>
-                    <Route path="/" component={showSetup ? Setup : Home} exact={true} />
-                </IonRouterOutlet>
-            </IonReactRouter>
+            {configLoading ? (
+                <IonLoading
+                    isOpen={configLoading}
+                    spinner="crescent"
+                    cssClass="ion-loading-custom"
+                    animated={true}
+                    showBackdrop={false}
+                    // onDidDismiss={() => }
+                />
+            ) : (
+                <IonReactRouter>
+                    <IonRouterOutlet>
+                        <Route
+                            path="/"
+                            render={() => (setupPage === null ? <Home /> : <Setup currentPage={setupPage} />)}
+                            exact={true}
+                        />
+                    </IonRouterOutlet>
+                </IonReactRouter>
+            )}
         </IonApp>
     )
 }
