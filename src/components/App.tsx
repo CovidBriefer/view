0d88df9496883efa838dbadf92f84546db3fd3b2
flaaaps <julianwening4@gmail.com>
2021-06-01 16:14:47 +0200

    Create App component with router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { Route } from "react-router-dom"
+import { IonReactRouter } from "@ionic/react-router"
+
+import { Storage } from "@capacitor/storage"
+// import Loading from "./Loading"
+import Setup from "./setup/Setup"
+import Home from "./Home"
+import { IonApp, IonRouterOutlet } from "@ionic/react"
 
 const App: React.FC = () => {
+    const [showSetup, setShowSetup] = useState(false)
+    useEffect(() => {
+        Storage.get({ key: "config" }).then(res => {
+            const data: { setup: boolean } = JSON.parse(res.value!!)
+            if (!data.setup) setShowSetup(true)
+        })
+    }, [])
     return (
-        <div>
-
-        </div>
+        <IonApp>
+            <IonReactRouter>
+                <IonRouterOutlet>
+                    <Route path="/" component={showSetup ? Setup : Home} exact={true} />
+                </IonRouterOutlet>
+            </IonReactRouter>
+        </IonApp>
     )
 }
 
