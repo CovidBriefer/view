3a9a570e782167c4c8f77a65878777f6cdfe8640
flaaaps <julianwening4@gmail.com>
2021-06-01 21:02:58 +0200

    Add start button & improve overall ui

--- src/components/pages/setup/Setup.tsx
+++ src/components/pages/setup/Setup.tsx
@@ -1,23 +1,25 @@
 import React from "react"
-import icon128 from "../../assets/icons/icon-128.svg"
+import icon128 from "../../../assets/icons/icon-128.svg"
 import PageLayout from "../../PageLayout"
+
+import Button from "../../Button"
 
 interface Props {}
 
 const Setup: React.FC<Props> = props => {
     return (
-        <PageLayout>
-            <div className="flex flex-col items-center justify-center">
-                <div>
-                    <img src={icon128} width="100" height="100" alt="Icon" />
-                    <h1 className="text-4xl font-bold">
+        <PageLayout className="ml-auto w-screen">
+            <div className="setup-start-wrapper h-2/3 px-5 flex flex-col items-center justify-between">
+                <div className="flex flex-col items-center mt-5">
+                    <img src={icon128} width="130" alt="Icon" />
+                    <h1 className="text-4xl font-bold mt-5 tracking-tighter">
                         Covid<span className="text-primary">Briefer</span>
                     </h1>
-                    <p className="text-light-gray text-center">
-                        Inzidenz, Tagesnews und mehr. Alle Informationen auf einen Blick.
+                    <p className="text-light-gray text-l text-center leading-5 mt-2">
+                        Inzidenz, Tagesnews und mehr! Alle Informationen auf einen Blick.
                     </p>
                 </div>
-                <button></button>
+                <Button>Los geht’s</Button>
             </div>
         </PageLayout>
     )
