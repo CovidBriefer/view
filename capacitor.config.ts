c4eb3c626331c17a3bdf6cb05495d2b2f075b355
flaaaps <julianwening4@gmail.com>
2021-06-01 12:56:15 +0200

    Generate capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'me.wening.covidbriefer',
+  appName: 'Covid Briefer',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
