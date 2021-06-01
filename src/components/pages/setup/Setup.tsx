10b4102e92240f9e53ed895f3b2aea267d51450a
flaaaps <julianwening4@gmail.com>
2021-06-01 21:20:19 +0200

    Update page on click

--- src/components/pages/setup/Setup.tsx
+++ src/components/pages/setup/Setup.tsx
@@ -1,26 +1,18 @@
-import React from "react"
-import icon128 from "../../../assets/icons/icon-128.svg"
+import React, { useState } from "react"
 import PageLayout from "../../PageLayout"
-
-import Button from "../../Button"
+import Introduction from "./Introduction"
 
 interface Props {}
 
+export type Page = "introduction" | "configuration"
 const Setup: React.FC<Props> = props => {
+    const [currentPage, setCurrentPage] = useState<Page>("introduction")
+
+    const updatePage = (page: Page) => setCurrentPage("configuration")
+
     return (
         <PageLayout className="ml-auto w-screen">
-            <div className="setup-start-wrapper h-2/3 px-5 flex flex-col items-center justify-between">
-                <div className="flex flex-col items-center mt-5">
-                    <img src={icon128} width="130" alt="Icon" />
-                    <h1 className="text-4xl font-bold mt-5 tracking-tighter">
-                        Covid<span className="text-primary">Briefer</span>
-                    </h1>
-                    <p className="text-light-gray text-l text-center leading-5 mt-2">
-                        Inzidenz, Tagesnews und mehr! Alle Informationen auf einen Blick.
-                    </p>
-                </div>
-                <Button>Los geht’s</Button>
-            </div>
+            {currentPage === "introduction" ? <Introduction updatePage={updatePage} /> : "Config"}
         </PageLayout>
     )
 }
