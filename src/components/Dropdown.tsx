ac8e598d15c53d47345a49c179a98c7e0f158126
flaaaps <julianwening4@gmail.com>
2021-06-02 20:06:17 +0200

    Small ui adjustments & add callback and header props

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,11 +1,13 @@
 import { motion, Variants } from "framer-motion"
 import { AnimationDefinition } from "framer-motion/types/render/utils/animation"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { FaAngleDown, FaAngleUp } from "react-icons/fa"
 
 interface Props {
     heading: JSX.Element
     list: any[]
+    initialHeader?: string
+    callback: (selection: any) => void
 }
 
 const variants: Variants = {
@@ -17,20 +19,28 @@ const variants: Variants = {
     },
 }
 
-const Dropdown: React.FC<Props> = ({ heading, list: initialList = [] }) => {
+const Dropdown: React.FC<Props> = ({ heading, list: initialList, initialHeader, callback }) => {
     const [isListOpen, setIsListOpen] = useState(false)
-    const [headerTitle, setHeaderTitle] = useState("Select an item")
+    const [headerTitle, setHeaderTitle] = useState(initialHeader || "Select an item")
     const [listDisplay, setListDisplay] = useState<"hidden" | "show">("hidden")
-    const [list, setList] = useState(initialList)
+    const [list, setList] = useState<any[]>([])
 
-    const selectItem = (item: Item) => {
-        setHeaderTitle(item.title)
+    useEffect(() => {
+        console.log("List:", initialList)
+        setList(initialList)
+        setHeaderTitle(initialHeader || "Select an item")
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [initialList])
+
+    const selectItem = (item: any) => {
+        setHeaderTitle(item.name)
+        setIsListOpen(false)
         let temp: List = [...list]
         temp.map(i => {
             return item.id !== i.id ? (i.selected = false) : (i.selected = true)
         })
         setList(temp)
-        setIsListOpen(false)
+        callback(item)
     }
 
     const toggleList = () => {
@@ -41,24 +51,26 @@ const Dropdown: React.FC<Props> = ({ heading, list: initialList = [] }) => {
         setListDisplay(type as "hidden" | "show")
     }
     return (
-        <div className="dd-wrapper mt-10 w-5/6 mx-auto">
+        <div className="dd-wrapper mt-10 w-5/6 mx-auto relative">
             {heading}
             <div
                 onClick={toggleList}
-                className="dd-header border-b-4 border-primary-dark bg-bg-light py-2 px-4 w-full text-base flex justify-between items-center mx-auto focus:outline-none"
+                className="dd-header text-base border-b-4 border-primary-dark bg-bg-light py-1 px-4 w-full flex justify-between items-center mx-auto focus:outline-none"
             >
                 <div className="dd-header-title">{headerTitle}</div>
-                <span className="ml-3">{isListOpen ? <FaAngleUp /> : <FaAngleDown />}</span>
+                <span>{isListOpen ? <FaAngleUp /> : <FaAngleDown />}</span>
             </div>
             <motion.div
                 variants={variants}
                 animate={isListOpen ? "show" : "hidden"}
                 onAnimationComplete={listAnimationComplete}
-                transition={{ duration: 0.25, delay: 0.1 }}
+                transition={{ duration: 0.15, delay: 0.1 }}
                 initial="hidden"
                 exit="hidden"
                 role="list"
-                className={"dd-list max-h-32 overflow-auto " + (listDisplay === "hidden" ? "hidden" : "block")}
+                className={
+                    "dd-list max-h-32 overflow-auto w-full z-50 " + (listDisplay === "hidden" ? "hidden" : "absolute")
+                }
             >
                 {list.map(
                     item =>
@@ -70,7 +82,7 @@ const Dropdown: React.FC<Props> = ({ heading, list: initialList = [] }) => {
                                     "bg-bg-light py-2 px-4 w-full text-base flex justify-between border-b-2 border-primary-dark items-center mx-auto focus:outline-none"
                                 }
                             >
-                                <div className="dd-list-item">{item.title}</div>
+                                <div className="dd-list-item">{item.name}</div>
                             </div>
                         )
                 )}
