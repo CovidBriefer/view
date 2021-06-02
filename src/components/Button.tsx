a4df191d4061711255e81e037510d2dfdbd73eae
flaaaps <julianwening4@gmail.com>
2021-06-02 20:05:23 +0200

    Adjust design & add className to props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,16 @@ import React from "react"
 
 interface Props {
     onClick?: () => void
+    className?: string
 }
 
-const Button: React.FC<Props> = ({ children, onClick }) => {
+const Button: React.FC<Props> = ({ children, onClick, className }) => {
     return (
         <button
             onClick={() => onClick?.()}
-            className="bg-primary py-2 px-20 text-primary-dark font-semibold tracking-tighter text-lg shadow-2xl"
+            className={
+                "bg-primary py-1 px-20 text-primary-dark font-semibold tracking-tighter text-lg shadow-2xl " + className
+            }
             style={{ borderRadius: "3px", filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))" }}
         >
             {children}
