e9832bd2ecae6f51e09f51c8c59c4e22d2541f42
flaaaps <julianwening4@gmail.com>
2021-06-02 13:36:39 +0200

    Start with configuration page

---
+++ src/components/pages/setup/Configuration.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../../Header"
+
+interface Props {}
+
+const Configuration: React.FC<Props> = props => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Configuration
