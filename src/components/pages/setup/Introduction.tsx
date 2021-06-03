b5c0d59e7d7b5fe418e1ce7eaefedc9d6fa0367f
flaaaps <julianwening4@gmail.com>
2021-06-03 12:38:24 +0200

    Update button positioning

--- src/components/pages/setup/Introduction.tsx
+++ src/components/pages/setup/Introduction.tsx
@@ -9,18 +9,24 @@ interface Props {
 
 const Introduction: React.FC<Props> = ({ updatePage }) => {
     return (
-        <div className="setup-start-wrapper h-2/3 px-5 flex flex-col items-center justify-between">
-            <div className="flex flex-col items-center mt-5">
-                <img src={icon128} width="130" alt="Icon" />
-                <h1 className="text-4xl font-bold mt-5 tracking-tighter">
-                    Covid<span className="text-primary">Briefer</span>
-                </h1>
-                <p className="text-light-gray text-l text-center leading-5 mt-2">
-                    Inzidenz, Tagesnews und mehr! Alle Informationen auf einen Blick.
-                </p>
+        <>
+            <div className="setup-start-wrapper h-3/6 px-5 flex flex-col items-center justify-start">
+                <div className="flex flex-col items-center mt-5">
+                    <img src={icon128} width="130" alt="Icon" />
+                    <h1 className="text-4xl font-bold mt-5 tracking-tighter">
+                        Covid<span className="text-primary">Briefer</span>
+                    </h1>
+                    <p className="text-light-gray text-l text-center leading-5 mt-2">
+                        Inzidenz, Tagesnews und mehr! Alle Informationen auf einen Blick.
+                    </p>
+                </div>
             </div>
-            <Button onClick={() => updatePage("configuration")}>Los geht’s</Button>
-        </div>
+            <div className="absolute bottom-12 w-full flex justify-center">
+                <Button className="mx-auto" onClick={() => updatePage("configuration")}>
+                    Los geht’s
+                </Button>
+            </div>
+        </>
     )
 }
 
