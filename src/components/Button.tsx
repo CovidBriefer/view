bc34b1a99384342db88a62ca59be2f1fdb0cef8e
flaaaps <julianwening4@gmail.com>
2021-06-03 13:43:51 +0200

    Style disabled button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,30 @@ import React from "react"
 interface Props {
     onClick?: () => void
     className?: string
+    disabled?: boolean
 }
 
-const Button: React.FC<Props> = ({ children, onClick, className }) => {
+const Button: React.FC<Props> = ({ children, onClick, className, disabled = false }) => {
     return (
         <button
+            disabled={disabled}
             onClick={() => onClick?.()}
             className={
-                "bg-primary py-1 px-20 text-primary-dark font-semibold tracking-tighter text-lg shadow-2xl " + className
+                "setup-button bg-primary py-1 px-20 focus:outline-none text-primary-dark font-semibold tracking-tighter text-lg shadow-2xl " +
+                className
             }
-            style={{ borderRadius: "3px", filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))" }}
+            style={
+                !disabled
+                    ? {
+                          borderRadius: "3px",
+                          filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))",
+                      }
+                    : {
+                          backgroundColor: "rgba(86, 206, 192, 0.5",
+                          borderRadius: "3px",
+                          filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))",
+                      }
+            }
         >
             {children}
         </button>
