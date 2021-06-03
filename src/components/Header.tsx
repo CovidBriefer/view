2d7f26007150f2dd57b99aba97486fd6241f026c
flaaaps <julianwening4@gmail.com>
2021-06-03 20:53:40 +0200

    Add settings icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import React from "react"
 import icon64 from "../assets/icons/icon-64.svg"
+import { FaCog } from "react-icons/fa"
 
-const Header: React.FC = () => {
+type Props = {
+    showSettingsIcon?: boolean
+}
+
+const Header: React.FC<Props> = ({ showSettingsIcon = false }) => {
     return (
-        <div className="mt-16 px-10">
-            <img src={icon64} width="39" alt="Icon" />
+        <div className="mt-16 px-10 flex items-center justify-between">
+            <img src={icon64} width="40" alt="Icon" />
+            {showSettingsIcon && <FaCog fontSize={25} color="#D3D3D3" />}
         </div>
     )
 }
