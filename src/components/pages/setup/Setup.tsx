dcd26b993f4ecc5413ed3790c806420596246262
flaaaps <julianwening4@gmail.com>
2021-06-03 12:39:07 +0200

    Rework page animations @incxption

--- src/components/pages/setup/Setup.tsx
+++ src/components/pages/setup/Setup.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react"
 import { Config, Page } from "../../.."
 import PageLayout from "../../PageLayout"
 import Introduction from "./Introduction"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import Configuration from "./Configuration"
 import { StorageContext } from "../../App"
 import useStorage from "../../hooks/useStorage"
@@ -36,9 +36,31 @@ const Setup: React.FC = () => {
 
     return (
         <PageLayout className="ml-auto w-screen">
-            <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ delay: 0.1 }}>
-                {currentPage === "introduction" ? <Introduction updatePage={updatePage} /> : <Configuration />}
-            </motion.div>
+            <AnimatePresence key={window.location.pathname} initial={true}>
+                {currentPage === "introduction" ? (
+                    <motion.div
+                        animate={{ opacity: 1 }}
+                        exit={{ x: "-100%", transition: { duration: 0.3 } }}
+                        initial={{ opacity: 0 }}
+                        key="intro"
+                        className="absolute top-0 left-0 w-full h-full"
+                        transition={{ delay: 0.2, duration: 0.3 }}
+                    >
+                        <Introduction updatePage={updatePage} />
+                    </motion.div>
+                ) : (
+                    <motion.div
+                        animate={{ x: 0 }}
+                        exit={{ x: "100%", transition: { duration: 0.3 } }}
+                        key="config"
+                        className="absolute top-0 left-0 w-full h-full"
+                        initial={{ x: "100%" }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        <Configuration />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </PageLayout>
     )
 }
