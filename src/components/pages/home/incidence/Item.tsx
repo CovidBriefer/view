31fd24fc4da22716f0349a063544275eac77210c
flaaaps <julianwening4@gmail.com>
2021-06-05 15:02:02 +0200

    Correctly fetch item data & retrieve incidence history

--- src/components/pages/home/incidence/Item.tsx
+++ src/components/pages/home/incidence/Item.tsx
@@ -1,26 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import arrowUp from "../../../../assets/icons/arrow-up.svg"
 import arrowDown from "../../../../assets/icons/arrow-down.svg"
-import { IncidenceData } from "./Overview"
-import useCovidHistory, { Params } from "../../../hooks/useCovidHistory"
-
-export type IncidenceType = "germany" | "state" | "district"
+import { IncidenceItem } from "./Overview"
+// import useCovidHistory, { Params } from "../../../hooks/useCovidHistory"
+import axios from "axios"
+import useCovidHistory from "../../../hooks/useCovidHistory"
 
 interface Props {
-    type: IncidenceType
-    data: IncidenceData
+    data: IncidenceItem
 }
 
-const Item: React.FC<Props> = ({ data, type }) => {
-    const params_ = { incidenceData: { weekIncidence: data.weekIncidence } }
-    const params =
-        type === "district"
-            ? { ...params_, type: "district", ags: data.ags! }
-            : type === "state"
-            ? { ...params_, type: "state", abbreviation: data.abbreviation! }
-            : { ...params_, type: "germany" }
+interface IncidenceDataItem extends IncidenceItem {
+    weekIncidence: number
+}
 
-    const { diffType, difference } = useCovidHistory(params as Params)
+const Item: React.FC<Props> = ({ data }) => {
+    const [incidenceData, setIncidenceData] = useState<IncidenceDataItem>()
+    const { diffType, difference, loading } = useCovidHistory({
+        type: data.type,
+        param: data.type === "district" ? data.ags! : data.type === "state" ? data.abbreviation! : "",
+        incidenceData: { weekIncidence: incidenceData?.weekIncidence },
+    })
+
+    const fetchIncidenceData = () => {
+        let url = "https://covidapi-cb.wening.me"
+        if (data.type === "district") {
+            url += `/districts/${data.ags}`
+        } else if (data.type === "state") url += `/states/${data.stateAbbreviation}`
+        else url += "/germany"
+        axios.get(url).then(res => {
+            if (data.type === "district" || data.type === "state") {
+                setIncidenceData(res.data.data[data.ags! ?? data.abbreviation])
+            } else {
+                setIncidenceData(res.data)
+            }
+        })
+    }
+
+    useEffect(() => {
+        fetchIncidenceData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <div className="bg-bg-light px-5 py-3 my-3 flex items-center justify-between rounded" style={{ width: "100%" }}>
@@ -28,11 +48,13 @@ const Item: React.FC<Props> = ({ data, type }) => {
                 className="text-lg text-light-gray font-medium m-0 w-1/2"
                 style={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
             >
-                {data.name}
+                {incidenceData?.name ?? data.name}
             </h3>
             <div className="flex">
-                <h1 className="text-2xl my-1 font-bold mr-4 m-0">{Math.round(data.weekIncidence * 10) / 10}</h1>
-                {difference !== 0 && (
+                <h1 className="text-2xl my-1 font-bold mr-4 m-0">
+                    {Math.round((incidenceData?.weekIncidence ?? 0) * 10) / 10}
+                </h1>
+                {difference !== 0 && !loading && (
                     <h4 className="m-0 flex items-center font-semibold">
                         <img
                             src={diffType === "inc" ? arrowUp : arrowDown}
