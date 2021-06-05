c8793653a87ac9e8704b75fc9886e86ea5a710dc
flaaaps <julianwening4@gmail.com>
2021-06-05 15:01:05 +0200

    Completely rework hook

--- src/components/hooks/useCovidHistory.tsx
+++ src/components/hooks/useCovidHistory.tsx
@@ -1,62 +1,45 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { IncidenceType } from "../pages/home/incidence/Overview"
 
-export type Params = (StateParams | DistrictParams | FederationParams) & { incidenceData: { weekIncidence: number } }
-
-type StateParams = {
-    type: "state"
-    abbreviation: string
+export type HistoryParams = {
+    type: IncidenceType
+    param: string
+} & {
+    incidenceData: { weekIncidence?: number }
 }
-type DistrictParams = {
-    type: "district"
-    ags: string
-}
-type FederationParams = {
-    type: "germany"
-}
-
-export default function useCovidHistory(params: Params) {
+export default function useCovidHistory(options: HistoryParams) {
     const [difference, setDifference] = useState(0)
     const [diffType, setDiffType] = useState<"inc" | "dec">("inc")
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        console.log("Use covid history fired! Mann über Bord!", params)
-        if (params.type === "germany") {
-            axios.get("https://covidapi-cb.wening.me/germany/history/incidence/2").then(res => {
-                const data = res.data.data
-                const pastIncidence = data[0].weekIncidence
-                console.log(pastIncidence, "PI")
-                const diff = pastIncidence - params.incidenceData.weekIncidence
-                console.log(
-                    `Past incidence: ${pastIncidence}; Current incidence: ${params.incidenceData.weekIncidence}`
-                )
-                setDifference(Math.abs(diff))
-                setDiffType(diff > 0 ? "dec" : "inc")
-            })
-        } else if (params.type === "state" || params.type === "district") {
-            console.log("Got district or state")
-            axios
-                .get(
-                    `https://covidapi-cb.wening.me/districts/${
-                        params.type === "state" ? params.abbreviation : params.ags
-                    }/history/incidence/2`
-                )
-                .then(res => {
+        if (!!options.incidenceData.weekIncidence) {
+            if (options.type === "germany") {
+                axios.get("https://covidapi-cb.wening.me/germany/history/incidence/2").then(res => {
                     const data = res.data.data
-                    const obj = data[Object.keys(data)[0]]
-                    const history = obj.history
-                    console.log("History:", history)
-                    const pastIncidence = history[0].weekIncidence
-                    const diff = pastIncidence - params.incidenceData.weekIncidence
-                    console.log(
-                        `Past incidence: ${pastIncidence}; Current incidence: ${params.incidenceData.weekIncidence}`
-                    )
+                    const pastIncidence = data[0].weekIncidence
+                    const diff = pastIncidence - options.incidenceData.weekIncidence!
                     setDifference(Math.abs(diff))
                     setDiffType(diff > 0 ? "dec" : "inc")
                 })
+            } else if (options.type === "state" || options.type === "district") {
+                axios
+                    .get(`https://covidapi-cb.wening.me/${options.type}s/${options.param}/history/incidence/2`)
+                    .then(res => {
+                        const data = res.data.data
+                        const obj = data[Object.keys(data)[0]]
+                        const history = obj.history
+                        const pastIncidence = history[0].weekIncidence
+                        const diff = pastIncidence - options.incidenceData.weekIncidence!
+                        setDifference(Math.abs(diff))
+                        setDiffType(diff > 0 ? "dec" : "inc")
+                    })
+            }
+            setLoading(false)
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    }, [options.incidenceData.weekIncidence])
 
-    return { diffType, difference }
+    return { diffType, difference, loading }
 }
