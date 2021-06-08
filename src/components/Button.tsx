ada594f9474967621d2f19683f2d058c499afa79
flaaaps <julianwening4@gmail.com>
2021-06-08 20:04:32 +0200

    Move box-shadow into class

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,27 +4,27 @@ interface Props {
     onClick?: () => void
     className?: string
     disabled?: boolean
+    shadow?: boolean
 }
 
-const Button: React.FC<Props> = ({ children, onClick, className, disabled = false }) => {
+const Button: React.FC<Props> = ({ children, onClick, className, disabled = false, shadow = true }) => {
     return (
         <button
             disabled={disabled}
-            onClick={() => onClick?.()}
+            onClick={() => !disabled && onClick?.()}
             className={
                 "setup-button bg-primary py-1 px-20 focus:outline-none text-primary-dark font-semibold tracking-tighter text-lg shadow-2xl " +
+                (shadow && " setup-button_shadow") +
                 className
             }
             style={
                 !disabled
                     ? {
-                          borderRadius: "3px",
-                          filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))"
+                          borderRadius: "3px"
                       }
                     : {
-                          backgroundColor: "rgba(86, 206, 192, 0.5",
-                          borderRadius: "3px",
-                          filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.25))"
+                          opacity: 0.5,
+                          borderRadius: "3px"
                       }
             }
         >
