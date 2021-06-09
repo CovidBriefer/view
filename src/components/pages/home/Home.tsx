5b98a3d9c1516c9253d3c8dbcfefa4b0f685f36e
flaaaps <julianwening4@gmail.com>
2021-06-09 15:59:30 +0200

    Push to index route if setup is not finished

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
+import useStorage from "../../../hooks/useStorage"
 import Header from "../../Header"
 import PageLayout from "../../PageLayout"
 import Overview from "./incidence/Overview"
+import { History } from "history"
 
-const Home: React.FC = () => {
+const Home: React.FC<{ history?: History<unknown> }> = ({ history }) => {
+    const { data: doneSetup, loading } = useStorage("config.setup.done", true)
+    useEffect(() => {
+        if (!loading && !doneSetup) history?.push("/")
+    }, [doneSetup, history, loading])
     return (
         <PageLayout className="mt-2">
             <div className="px-3 mx-auto">
