8dd2b055615d8bf6e87ee436fead213c2b307ac2
flaaaps <julianwening4@gmail.com>
2021-06-10 15:25:43 +0200

    Handle item selection

--- src/components/pages/home/incidence/AppendItemOverlay.tsx
+++ src/components/pages/home/incidence/AppendItemOverlay.tsx
@@ -3,26 +3,29 @@ import React, { useEffect, useState } from "react"
 import Button, { ClickEvent } from "../../../Button"
 import Dropdown from "../../../Dropdown"
 import { IoIosClose } from "react-icons/io"
-import useCovidApi, { State } from "../../../../hooks/useCovidApi"
+import useCovidApi, { District, State } from "../../../../hooks/useCovidApi"
 import { formatStates } from "../../../../utils/format"
 
 interface Props {
     setShowAppendModal: (show: boolean) => void
+    addIncidenceItem: (item: District | State) => void
 }
 
-const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal }) => {
-    const [currentSection, setCurrentSection] = useState<"type-selection" | "selection">("type-selection")
+const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceItem }) => {
+    const [currentSection, setCurrentSection] = useState<"selection" | "configuration">("selection")
     const [states, setStates] = useState<State[]>([])
     const { response, loading, error } = useCovidApi({
         method: "GET",
         url: "/states"
     })
 
+    const [selectedItem, setSelectedItem] = useState<State | District>()
+
     // const { response: _districts, loading: _districtsLoading } = useCovidApi({ method: "GET", url: "/districts" })
     const [type, setType] = useState<"state" | "district">()
     const handleTypeChange = (type: "state" | "district") => {
         setType(type)
-        setCurrentSection("selection")
+        setCurrentSection("configuration")
     }
 
     useEffect(() => {
@@ -31,6 +34,10 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal }) => {
             setStates(formatStates(response.data))
         }
     }, [response, loading, error])
+
+    const handleSelectionChange = (selection: any) => {
+        setSelectedItem(selection)
+    }
     return (
         <motion.div
             className="bg-bg-light fixed py-10 w-full bottom-0 left-0 px-7"
@@ -42,25 +49,25 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal }) => {
             initial={{ y: "100%" }}
             exit={{ y: "120%" }}
         >
+            <motion.span
+                animate={{ opacity: 1, transition: { delay: 0.15 } }}
+                initial={{ opacity: 0 }}
+                exit={{ y: "-100%" }}
+                onClick={() => setShowAppendModal(false)}
+            >
+                <IoIosClose
+                    size={20}
+                    style={{ transform: "scale(2.5)" }}
+                    className="mb-0 float-right fixed right-6 top-6"
+                />
+            </motion.span>
             <AnimatePresence exitBeforeEnter>
-                <motion.span
-                    animate={{ opacity: 1, transition: { delay: 0.15 } }}
-                    initial={{ opacity: 0 }}
-                    exit={{ y: "-100%" }}
-                    onClick={() => setShowAppendModal(false)}
-                >
-                    <IoIosClose
-                        size={20}
-                        style={{ transform: "scale(2.5)" }}
-                        className="mb-0 float-right fixed right-6 top-6"
-                    />
-                </motion.span>
-                {currentSection === "type-selection" ? (
+                {currentSection === "selection" ? (
                     <motion.div
                         key="type-selection"
                         animate={{ y: "0%", transition: { delay: 0.05 } }}
                         initial={{ y: "50%" }}
-                        exit={{ x: "-110%" }}
+                        exit={{ scale: 0.9 }}
                         className="flex flex-col items-center"
                     >
                         <h1 className="font-semibold text-3xl m-0">Hinzufügen</h1>
@@ -73,8 +80,8 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal }) => {
                 ) : (
                     <motion.div
                         key="selection"
-                        animate={{ x: "0%" }}
-                        initial={{ x: "110%" }}
+                        animate={{ scale: 1 }}
+                        initial={{ scale: 0.9 }}
                         className="flex flex-col items-center"
                     >
                         {type === "state" ? (
@@ -84,14 +91,17 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal }) => {
                                     Dir wird die 7-Tage-Inzidenz und die Live-Entwicklung in deinem Feed angezeigt.
                                 </h3>
                                 <Dropdown
-                                    callback={s => console.log("Selected:", s)}
+                                    callback={handleSelectionChange}
                                     list={[...states]}
                                     heading=""
                                     dropShadow="drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.15))"
                                     initialHeader="Wähle ein Bundesland"
                                     orientation="top"
                                 />
-                                <AppendButton name="Hinzufügen" />
+                                <AppendButton
+                                    name="Hinzufügen"
+                                    onClick={() => selectedItem && addIncidenceItem(selectedItem)}
+                                />
                             </>
                         ) : (
                             ""
@@ -103,7 +113,7 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal }) => {
     )
 }
 
-const AppendButton: React.FC<{ name: string; onClick?: (e: ClickEvent) => void }> = ({ name, onClick }) => (
+export const AppendButton: React.FC<{ name: string; onClick?: (e: ClickEvent) => void }> = ({ name, onClick }) => (
     <Button onClick={onClick} className="cursor-pointer flex justify-center items-center mt-5 w-full">
         <span className="text-bg-light w-full">{name}</span>
         {/* <FaArrowRight fill="rgb(31, 65, 96)" /> */}
