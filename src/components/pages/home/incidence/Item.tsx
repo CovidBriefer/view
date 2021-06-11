b14fb6d2d2a155850c539af90939acbea5bdc492
flaaaps <julianwening4@gmail.com>
2021-06-11 23:05:37 +0200

    Allow to remove items

--- src/components/pages/home/incidence/Item.tsx
+++ src/components/pages/home/incidence/Item.tsx
@@ -5,16 +5,20 @@ import { IncidenceItem } from "./Overview"
 // import useCovidHistory, { Params } from "../../../hooks/useCovidHistory"
 import axios from "axios"
 import useCovidHistory from "../../../../hooks/useCovidHistory"
+import { motion } from "framer-motion"
+import { FaTrashAlt } from "react-icons/fa"
 
 interface Props {
     data: IncidenceItem
+    removeItem: (internalId: string) => void
 }
 
 interface IncidenceDataItem extends IncidenceItem {
     weekIncidence: number
 }
 
-const Item: React.FC<Props> = ({ data }) => {
+const Item: React.FC<Props> = ({ data, removeItem }) => {
+    const [showBin, setShowBin] = useState(false)
     const [incidenceData, setIncidenceData] = useState<IncidenceDataItem>()
     const { diffType, difference, loading } = useCovidHistory({
         type: data.type,
@@ -22,7 +26,7 @@ const Item: React.FC<Props> = ({ data }) => {
         incidenceData: { weekIncidence: incidenceData?.weekIncidence }
     })
 
-    const fetchIncidenceData = () => {
+    const fetchIncidenceData = async () => {
         let url = "https://covidapi-cb.wening.me"
         if (data.type === "district") {
             url += `/districts/${data.ags}`
@@ -42,39 +46,63 @@ const Item: React.FC<Props> = ({ data }) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const handlePan = (pointInfo: any) => {
+        const valid = pointInfo.offset.x < 0
+        if (valid !== showBin) {
+            setShowBin(pointInfo.offset.x < 0)
+        }
+    }
+
     return (
-        <div
-            className="bg-bg-light px-5 py-3 my-4 flex items-center justify-between rounded"
-            style={{ width: "100%", filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.075))" }}
-        >
-            <h3
-                className="text-lg text-light-gray font-medium m-0 w-1/2"
-                style={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
+        <div className="overflow-hidden flex justify-center items-center">
+            <motion.div
+                animate={showBin ? { x: -35 } : { x: 0 }}
+                onPan={(_, pointInfo) => handlePan(pointInfo)}
+                className="bg-bg-light px-5 py-3 my-4 flex items-center justify-between rounded"
+                style={{ width: "100%", filter: "drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.075))" }}
             >
-                {incidenceData?.name ?? data.name}
-            </h3>
-            <div className="flex">
-                <h1 className="text-2xl my-1 font-bold mr-4 m-0">
-                    {Math.round((incidenceData?.weekIncidence ?? 0) * 10) / 10}
-                </h1>
-                {difference !== 0 && !loading && (
-                    <h4 className="m-0 flex items-center font-semibold">
-                        <img
-                            src={diffType === "inc" ? arrowUp : arrowDown}
-                            width="21"
-                            alt={diffType === "dec" ? "Absteigend" : "Aufsteigend"}
-                            className="mr-2"
-                        />
-                        <p
-                            className={
-                                (diffType === "inc" ? "text-increasing" : "text-decreasing") + " text-lg font-semibold"
-                            }
-                        >
-                            {Math.round(difference * 10) / 10}
-                        </p>
-                    </h4>
-                )}
-            </div>
+                <h3
+                    className="text-lg text-light-gray font-medium m-0 w-1/2"
+                    style={{ textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}
+                >
+                    {incidenceData?.name ?? data.name}
+                </h3>
+                <div className="flex">
+                    <h1 className="text-2xl my-1 font-bold mr-4 m-0">
+                        {Math.round((incidenceData?.weekIncidence ?? 0) * 10) / 10}
+                    </h1>
+                    {difference !== 0 && !loading && (
+                        <h4 className="m-0 flex items-center font-semibold">
+                            <img
+                                src={diffType === "inc" ? arrowUp : arrowDown}
+                                width="21"
+                                alt={diffType === "dec" ? "Absteigend" : "Aufsteigend"}
+                                className="mr-2"
+                            />
+                            <p
+                                className={
+                                    (diffType === "inc" ? "text-increasing" : "text-decreasing") +
+                                    " text-lg font-semibold"
+                                }
+                            >
+                                {Math.round(difference * 10) / 10}
+                            </p>
+                        </h4>
+                    )}
+                </div>
+            </motion.div>
+            <motion.div
+                onClick={() => removeItem(data.internalId)}
+                className="m-0"
+                initial={{ x: 50, opacity: 1, display: "none", scale: 0.5 }}
+                animate={
+                    showBin
+                        ? { x: -20, opacity: 1, scale: 1, display: "block" }
+                        : { x: 20, opacity: 1, display: "none", scale: 0.5 }
+                }
+            >
+                <FaTrashAlt size={25} fill="#e74c3c" />
+            </motion.div>
         </div>
     )
 }
