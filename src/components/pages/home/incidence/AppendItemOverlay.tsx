75a45092dc88da581c8a9464d44bba4bf80239e7
flaaaps <julianwening4@gmail.com>
2021-06-12 22:34:25 +0200

    Rename item if "Landkreis" and "Stadtkreis" is provided

--- src/components/pages/home/incidence/AppendItemOverlay.tsx
+++ src/components/pages/home/incidence/AppendItemOverlay.tsx
@@ -4,7 +4,7 @@ import Button, { ClickEvent } from "../../../Button"
 import Dropdown from "../../../Dropdown"
 import { IoIosClose } from "react-icons/io"
 import useCovidApi, { District, State } from "../../../../hooks/useCovidApi"
-import { formatStates, parseDistrictsByState } from "../../../../utils/format"
+import { formatStates, parseDistrictsByState, renameAmbiguousItems } from "../../../../utils/format"
 import { IncidenceItem } from "./Overview"
 
 interface Props {
@@ -37,14 +37,15 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceIt
 
     useEffect(() => {
         if (!loading) {
-            console.log("Got states:", response)
             setStates(formatStates(response.data))
         }
     }, [response, loading, error])
 
     useEffect(() => {
         if (!allDistrictsLoading && selectedState) {
-            setDistricts(parseDistrictsByState(allDistricts.data, selectedState))
+            const parsedDistricts = parseDistrictsByState(allDistricts.data, selectedState)
+            renameAmbiguousItems(parsedDistricts)
+            setDistricts(parsedDistricts)
         }
     }, [allDistricts, allDistrictsLoading, selectedState])
 
@@ -93,10 +94,10 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceIt
                         animate={{ y: "0%", transition: { delay: 0.05 } }}
                         initial={{ y: "50%" }}
                         exit={{ scale: 0.9 }}
-                        className="flex flex-col items-center"
+                        className="flex flex-col items-center h-64"
                     >
-                        <h1 className="font-semibold text-3xl m-0">Hinzufügen</h1>
-                        <h3 className="m-0 font-medium text-xl my-2 leading-6 text-center">
+                        <h1 className="font-semibold text-3xl m-0 mt-4">Hinzufügen</h1>
+                        <h3 className="m-0 font-medium text-xl my-2 mb-auto leading-6 text-center">
                             Möchtest du ein Bundesland oder einen Kreis hinzufügen?
                         </h3>
                         <AppendButton name="Bundesland" onClick={() => handleTypeChange("state")} />
@@ -107,13 +108,15 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceIt
                         key="selection"
                         animate={{ scale: 1 }}
                         initial={{ scale: 0.9 }}
-                        className="flex flex-col items-center"
+                        className="flex flex-col items-center h-64"
                     >
                         {type === "state" ? (
                             <>
                                 <h1 className="font-semibold mb-0">Bundesland</h1>
-                                {/* <h3>Füge ein Bundesland hinzu und</h3> */}
-                                <div className="w-full -mt-3">
+                                <h3 className="font-normal text-lg m-0 mt-1 text-center leading-5">
+                                    Wähle ein Bundesland um die Inzidenz in deinem Feed angezeigt zu bekommen.
+                                </h3>
+                                <div className="w-full -mt-3 mb-auto">
                                     <Dropdown
                                         callback={s => handleSelectionChange(s)}
                                         list={filterDuplicateItems(states)}
@@ -129,7 +132,7 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceIt
                                 />
                             </>
                         ) : (
-                            <>
+                            <div className="h-64 flex flex-col items-center w-full relative">
                                 <h1 className="font-semibold mb-0"> Land-/Stadtkreis</h1>
                                 <div className="w-full -my-4">
                                     <Dropdown
@@ -142,22 +145,25 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceIt
                                     />
                                 </div>
                                 <div className="w-full mb-3">
-                                    <Dropdown
-                                        callback={s => handleSelectionChange(s)}
-                                        list={districts}
-                                        heading=""
-                                        dropShadow="drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.15))"
-                                        initialHeader="Wähle einen Kreis"
-                                        orientation="top"
-                                    />
+                                    {selectedState && (
+                                        <Dropdown
+                                            callback={s => handleSelectionChange(s)}
+                                            list={districts}
+                                            heading=""
+                                            dropShadow="drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.15))"
+                                            initialHeader="Wähle einen Kreis"
+                                            orientation="top"
+                                        />
+                                    )}
                                 </div>
                                 <AppendButton
                                     name="Hinzufügen"
+                                    className="bottom-5 absolute"
                                     onClick={() =>
                                         selectedItem && addIncidenceItem({ ...selectedItem, type: "district" })
                                     }
                                 />
-                            </>
+                            </div>
                         )}
                     </motion.div>
                 )}
@@ -166,8 +172,15 @@ const AppendItemOverlay: React.FC<Props> = ({ setShowAppendModal, addIncidenceIt
     )
 }
 
-export const AppendButton: React.FC<{ name: string; onClick?: (e: ClickEvent) => void }> = ({ name, onClick }) => (
-    <Button onClick={onClick} className="cursor-pointer flex justify-center items-center mt-5 w-full">
+export const AppendButton: React.FC<{ name: string; onClick?: (e: ClickEvent) => void; className?: string }> = ({
+    name,
+    onClick,
+    className
+}) => (
+    <Button
+        onClick={onClick}
+        className={"cursor-pointer flex justify-center items-center mt-5 w-full" + (!!className ? ` ${className}` : "")}
+    >
         <span className="text-bg-light w-full">{name}</span>
         {/* <FaArrowRight fill="rgb(31, 65, 96)" /> */}
     </Button>
