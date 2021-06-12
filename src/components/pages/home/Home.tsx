e8afdd799595f9609a55da7cf2ab5e522e499b2a
flaaaps <julianwening4@gmail.com>
2021-06-12 20:17:32 +0200

    Set provider for App context

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -4,19 +4,23 @@ import Header from "../../Header"
 import PageLayout from "../../PageLayout"
 import Overview from "./incidence/Overview"
 import { History } from "history"
+import AppContextProvider from "../../../contexts/AppContext"
 
 const Home: React.FC<{ history?: History<unknown> }> = ({ history }) => {
     const { data: doneSetup, loading } = useStorage("config.setup.done", true)
+
     useEffect(() => {
         if (!loading && !doneSetup) history?.push("/")
     }, [doneSetup, history, loading])
     return (
-        <PageLayout className="mt-2">
-            <div className="px-3 mx-auto h-full">
-                <Header showSettingsIcon={false} />
-                <Overview />
-            </div>
-        </PageLayout>
+        <AppContextProvider>
+            <PageLayout className="mt-2">
+                <div className="px-3 mx-auto h-full">
+                    <Header showSettingsIcon={false} />
+                    <Overview />
+                </div>
+            </PageLayout>
+        </AppContextProvider>
     )
 }
 
